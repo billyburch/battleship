e497c5255997fb6219c4bb285aee167237574712
Billy Burch <billyburch40@gmail.com>
2023-09-08 16:48:26 -0600

    feat: add render method with optional argument)

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,8 +1,10 @@
 class Cell
-  attr_reader :coordinate, :ship
+  attr_reader :coordinate, :ship, :fired_upon
+
   def initialize(coordinate)
     @coordinate = coordinate
     @ship = nil
+    @fired_upon = false
   end
 
   def empty?
@@ -13,17 +15,55 @@ class Cell
     @ship = ship_name
   end
 
+  def fired_upon?
+    @fired_upon
+    # if @fire_upon 
+    #   true
+    # else
+    #   false
+    # end
+  end
+  
   def fire_upon
+    @fired_upon = true
     if !@ship.nil? 
-      ship.health -= 1
+      ship.hit
     end
   end
 
-  def fired_upon?
-    if @fire_upon >= 1
-      true
-    else
-      false
+  def render(reveal_ship = false)
+    return "S" if reveal_ship == true
+    # if @fired_upon
+    #   if !empty? and !@ship.nil?
+    #     return "S" if reveal_ship == true
+    #     @ship.sunk?
+    #     return "X"
+    #   elsif !@ship.nil?
+    #     return "H"
+    #   elsif @ship.nil?
+    #     return "M"
+    #   else
+    #     "."
+    #   end
+    # end
+    # if !@fired_upon
+    #   return "."
+    # elsif @fired_upon and @ship.nil?
+    #   return "M"
+    # elsif @fired_upon and @ship.hit
+    #   return "H"
+    # else @fired_upon and @ship.hit and @ship.sunk?
+    #   return "X"    
+    # end
+    
+    if @fired_upon and @ship.nil?
+      return "M"
+    elsif @fired_upon and !@ship.nil? and !@ship.sunk?
+      return "H"
+    elsif @fired_upon and @ship.sunk?
+      return "X"   
+    else !@fired_upon
+      return "."
     end
   end
 end
