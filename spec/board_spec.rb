90c2005b3e2c3ca24a657a9d893ccb931fb529cf
Billy Burch <billyburch40@gmail.com>
2023-09-10 16:09:40 -0600

    feat: add passing tests for valid_placement? method

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -34,21 +34,71 @@ RSpec.describe Board do
       .to be false
     end
 
-    xit 'splits string elements into letters/numbers' do
-      expect(@board.split_elements(@cruiser, ["A1", "A2"])).to eq([["A", "1"], ["A", "2"]])
-      expect(@board.split_elements(@cruiser, ["A1", "C4"])).to eq([["A", "1"], ["C", "4"]])
+    #tests with map and uniq methods/enumerables
+
+    it 'splits string elements into separate arrays of letters/numbers' do
+      expect(@board.get_letters(@cruiser, ["A1", "A2"])).to eq(["A", "A"])
+      expect(@board.get_numbers(@cruiser, ["A1", "A2"])).to eq([1, 2])
     end
 
-    xit 'coordinates are consecutive' do
+    it 'coordinate numbers are consecutive' do
+      expect(@board.consecutive_numbers?(@cruiser, ["A1", "A2", "A4"])).to be false
+      expect(@board.consecutive_numbers?(@submarine, ["A1", "C1"])).to be false
+      expect(@board.consecutive_numbers?(@cruiser, ["A3", "A2", "A1"])).to be false
+      expect(@board.consecutive_numbers?(@submarine, ["C1", "B1"])).to be false
+      #tests for true below
+      expect(@board.consecutive_numbers?(@submarine, ["A1", "A2"])).to be true
+      expect(@board.consecutive_numbers?(@cruiser, ["A2", "A3", "A4"])).to be true
+    end
+
+    it 'coordinate letters are consecutive' do
+      expect(@board.consecutive_letters?(@cruiser, ["A1", "A2", "C4"])).to be false
+      expect(@board.consecutive_letters?(@submarine, ["A1", "C1"])).to be false
+      expect(@board.consecutive_letters?(@cruiser, ["A3", "A2", "A1"])).to be false
+      expect(@board.consecutive_letters?(@submarine, ["C1", "B1"])).to be false
+      #tests for true below
+      expect(@board.consecutive_letters?(@submarine, ["A1", "B1"])).to be true
+      expect(@board.consecutive_letters?(@cruiser, ["A2", "B2", "C2"])).to be true
+    end
+
+    it 'checks rows and columns' do
+      expect(@board.same_row?(@cruiser, ["A1", "B2", "C3"])).to be false
+      expect(@board.same_column?(@cruiser, ["A1", "B2", "C3"])).to be false
+      expect(@board.same_row?(@submarine, ["A2", "A3", "A4"])).to be true
+      expect(@board.same_column?(@submarine, ["A2", "A3", "A4"])).to be false
+      expect(@board.same_row?(@submarine, ["A2", "B2", "C2"])).to be false
+      expect(@board.same_column?(@submarine, ["A2", "B2", "C2"])).to be true
+    end
+
+    it 'coordinates are consecutive' do
       expect(@board.valid_placement?(@cruiser, ["A1", "A2", "A4"])).to be false
       expect(@board.valid_placement?(@submarine, ["A1", "C1"])).to be false
       expect(@board.valid_placement?(@cruiser, ["A3", "A2", "A1"])).to be false
       expect(@board.valid_placement?(@submarine, ["C1", "B1"])).to be false
     end
 
-    xit 'coordinates are not diagonal' do
+    it 'coordinates are not diagonal' do
       expect(@board.valid_placement?(@cruiser, ["A1", "B2", "C3"])).to be false
       expect(@board.valid_placement?(@submarine, ["C2", "D3"])).to be false
     end
+
+    it 'valid placements return true' do
+      expect(@board.valid_placement?(@submarine, ["A1", "A2"])).to be true
+      expect(@board.valid_placement?(@cruiser, ["B1", "C1", "D1"])).to be true
+    end
   end
+
+  describe '#place' do
+    xit 'places a ship on the board in given cells' do
+      board.place(cruiser, ["A1", "A2", "A3"])
+      cell_1 = board.cells["A1"]
+      cell_2 = board.cells["A2"]
+      cell_3 = board.cells["A3"]
+      expect(cell_1.ship).to be_a(@cruiser)
+      expect(cell_2.ship).to be_a(@cruiser)
+      expect(cell_3.ship).to be_a(@cruiser)
+      expect(cell_3.ship == cell_2.ship).to be true
+    end
+  end
+
 end
